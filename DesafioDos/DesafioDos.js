5d3451254f70fd67224b6938b75722c6d590f9df
Selena <Selenac99@hotmail.com>
2023-11-01 14:05:07 -0300

    razonando xd

--- DesafioDos/DesafioDos.js
+++ DesafioDos/DesafioDos.js
@@ -34,7 +34,39 @@ W = VV
 X = ><
 Y = Ч
 Z = 2
-
-
-
+-Ahora lo que tengo que hacer es declarar cada variable con su respectivo lenguaje
+ejemplo let A = 4, tengo que hacer como una pregunta por ejemplo: Escriba una frase e imprimir la frase con el lenguaje de hacker
 */ 
+
+//PRIMER PASO: DECLARAR LAS VARIABLES
+let a = 4;
+let b = 13;
+let c = '[';
+let d  = 'cl';
+let e = 3;
+let f = '|=';
+let g = 6;
+let h = '|-|';
+let i = 1;
+let j = ',_|';
+let k = '|<';
+let l = '|_';
+let m = 'nn';
+let n = '/\/';
+let o = 0;
+let p = '|o';
+let q = '(_,)';
+let r = '/2';
+let s = '$';
+let t = 7;
+let u = 'v';
+let v = '\/';
+let w = 'VV';
+let x = '><';
+let y = 'Ч';
+let z = 2;
+
+//Segundo paso para mi hay que pedir que ingrese un texto:
+let nombre = prompt("Por favor, ingrese un texto el que quiera: ");
+        console.log(`Su texto que ingresó fue: ${nombre}`);
+        //console.log ("Ahora lo vamos a convertir en lenguaje hacker");
