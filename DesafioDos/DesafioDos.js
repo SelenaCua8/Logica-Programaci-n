55bcbc4aeadbce3a48249cd6dd4e92d2c450d1d6
Selena <Selenac99@hotmail.com>
2023-11-01 13:35:41 -0300

    agregando lenguaje hacker

---
+++ DesafioDos/DesafioDos.js
@@ -0,0 +1,40 @@
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+
+/*LENGUAJE HACKER
+A= 4
+B = 13
+C = [
+D = cl
+E = 3
+F = |=
+G = 6
+H = |-|
+I = 1
+J= ,_|
+K = |<
+L = |_
+M = nn
+N = /\/
+O = 0
+P = |o
+Q = (_,)
+R = /2
+S = $
+T = 7
+U = v
+V = \/
+W = VV
+X = ><
+Y = Ч
+Z = 2
+
+
+
+*/ 
