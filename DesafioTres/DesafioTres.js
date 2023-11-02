b211f4f1e2cc684bc7f8925eac05387af2649501
Selena <Selenac99@hotmail.com>
2023-11-02 16:06:05 -0300

    primer paso declaracion de array

--- DesafioTres/DesafioTres.js
+++ DesafioTres/DesafioTres.js
@@ -20,3 +20,8 @@
 
 //CUARTO PASO: Que lo devuelva a la funcion
 
+//Primer paso creo un array con los valores que quiero mezclar 
+const vocabulary = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '$', '%', '&', '/', '(', ')', '=', '?', '¿', '+', '-', '~'
+  ];
+  
