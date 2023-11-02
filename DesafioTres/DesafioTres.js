be6ec539c234e942dc1a83c04e07e19072cfc144
Selena <Selenac99@hotmail.com>
2023-11-02 15:49:48 -0300

    tercer desafio razonandolo y despues hacerlo en html y css

---
+++ DesafioTres/DesafioTres.js
@@ -0,0 +1,22 @@
+/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+
+//hay que hacer una funcion caracteres <=8 y >=16, que se pueda mayusculas y minisculuas, numeros y simbolos y que sea combinado. de forma aleatoria.
+// y como en el anterior una declaracion con todo el abecedario, los numeros y simbolos.
+//para mi es una function llamada paswordrandom()
+
+//PRIMER PASO: Declaración de variables
+
+//SEGUNDO PASO: Crear una función
+
+//TERCER PASO: Dentro de la funcion buscar algo que los mezcle a todas esas variables
+
+//CUARTO PASO: Que lo devuelva a la funcion
+
